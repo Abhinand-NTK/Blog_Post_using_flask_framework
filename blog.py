a498ad78c6919bbde4d85567b814721bb46d7710
AbhinandNtk <abhinandntkm@gmail.com>
2023-12-07 14:32:48 +0530

    Complete the idea about setting the envoirnment variable in flask for running the file

--- blog.py
+++ blog.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello this is abhinand, World!</p>"
+@app.route("/about")
+def about():
+    return "<p>This is the about page i have in my application!</p>"
+
+if __name__ == '__main__':
+        app.run(debug=True) 
