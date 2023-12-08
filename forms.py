336c765c94a3a25723df15c91b939b14a9d9c377
AbhinandNtk <abhinandntkm@gmail.com>
2023-12-08 12:42:30 +0530

    The forms and others files are added

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+class RegistrationFrom(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2, max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    Confirm_password = PasswordField('Confirm_password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+
+    Email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
